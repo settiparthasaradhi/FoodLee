5d0dc2fe72b90373a75d6c048945062da3146160
Partha Saradhi Setti <154002513+settiparthasaradhi@users.noreply.github.com>
2024-10-12 17:29:58 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 import "dotenv/config";
-export const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URL);
-};
 
-// add your mongoDB connection string above.
-// Do not use '@' symbol in your databse user's password else it will show an error.
+export const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://parthu0072:cnlbxQK78FDObov2@cluster0.66nvr.mongodb.net/FoodLee", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("db connected");
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
+};
