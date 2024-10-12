0c68ff77f4b833117b1516d3d920d463859e7e4d
Partha Saradhi Setti <154002513+settiparthasaradhi@users.noreply.github.com>
2024-10-12 17:38:50 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import "dotenv/config";
+
 export const connectDB = async () => {
-  await mongoose.connect("mongodb+srv://parthu0072:cnlbxQK78FDObov2@cluster0.66nvr.mongodb.net/FoodLee"
-    ).then(()=>console.log("db connected");
+  await mongoose.connect("mongodb+srv://parthu0072:cnlbxQK78FDObov2@cluster0.66nvr.mongodb.net/FoodLee")
+    .then(() => console.log("db connected"))
+    .catch((err) => console.error("Connection failed", err));
 };
