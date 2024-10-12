21801d14ab883f98cfdb475183377153850105c5
Partha Saradhi Setti <154002513+settiparthasaradhi@users.noreply.github.com>
2024-10-12 17:35:38 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,6 @@
 import mongoose from "mongoose";
 import "dotenv/config";
-
 export const connectDB = async () => {
-  try {
-    await mongoose.connect("mongodb+srv://parthu0072:cnlbxQK78FDObov2@cluster0.66nvr.mongodb.net/FoodLee", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("db connected");
-  } catch (error) {
-    console.error("Database connection error:", error);
-  }
+  await mongoose.connect("mongodb+srv://parthu0072:cnlbxQK78FDObov2@cluster0.66nvr.mongodb.net/FoodLee"
+    ).then(()=>console.log("db connected");
 };
